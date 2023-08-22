72a1234e61a3c8f99a4029e5bdc51e69bbd56869
rayrayrachel <142870008+rayrayrachel@users.noreply.github.com>
2023-08-22 14:00:29 +0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+/* Place your JavaScript in this file */
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("myNavbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
